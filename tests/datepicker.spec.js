ad8566a82a171fe1492023b3cee50a95c7a4826c
ramranjanjha89 <ramranjanjha89.rr@gmail.com>
2025-05-08 16:33:58 +0530

    conflict created

--- tests/datepicker.spec.js
+++ tests/datepicker.spec.js
@@ -7,6 +7,6 @@ test(' date picker', async ({ page })=>
     await page.goto('https://testautomationpractice.blogspot.com/');
      await page.locator('#start-date').click();
     // my name is lakhan
-
+    // my name is sajan
      await page.waitForTimeout(4000)
 })
