aeeb6af741ee32076a23d8f4d42e5c8acd76d0d2
ramranjanjha89 <ramranjanjha89.rr@gmail.com>
2025-05-08 15:59:09 +0530

    test updated

--- tests/datepicker.spec.js
+++ tests/datepicker.spec.js
@@ -6,6 +6,7 @@ test(' date picker', async ({ page })=>
 
     await page.goto('https://testautomationpractice.blogspot.com/');
      await page.locator('#start-date').click();
+    // my name is lakhan
 
      await page.waitForTimeout(4000)
 })
